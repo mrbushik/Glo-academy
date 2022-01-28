108835d00dd1d29f48f282c6e9c390af2ae57787
Nikita Bushuev <mrbushik1@gmail.com>
2022-01-28 19:22:40 +0300

    ready homework02

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-let title = "hello";
-let screens = 1080;
+let title = "lesson02";
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 999;
-const rollback = "back";
-const fullPrice = 1090;
-let adaptive = 320;
-console.log(title);
-alert(fullPrice);
+let rollback = "16";
+let fullPrice = 1090;
+let adaptive = false;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice +
+    '$ Стоимость разработки сайта ' + fullPrice + '$');
+//не уверен что лучше всего так разделять строку
+console.log(screens.toLowerCase().split());
+console.log(fullPrice * (rollback / 100));
