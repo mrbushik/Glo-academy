8778f83f2d8e6a891cc43fe48009c1c9459cb810
Nikita Bushuev <mrbushik1@gmail.com>
2022-01-29 12:03:07 +0300

    исправленное дз 2 урока

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ let screenPrice = 999;
 let rollback = "16";
 let fullPrice = 1090;
 let adaptive = false;
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
+console.log('Тип данных title: ' + typeof title);
+console.log('Тип данных fullPrice: ' + typeof fullPrice);
+console.log('Тип данных adaptive: ' + typeof adaptive);
+console.log('Длина строки screens = ' + screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice +
     '$ Стоимость разработки сайта ' + fullPrice + '$');
-//не уверен что лучше всего так разделять строку
-console.log(screens.toLowerCase().split());
-console.log(fullPrice * (rollback / 100));
+console.log(screens.toLowerCase().split(' '));
+let workerRollback = (fullPrice * (rollback / 100));
+console.log('Откат посреднику за работу: ' + Math.round(workerRollback));
