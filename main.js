dc4449d9052ffee5f5bf967ef1a66f693334fad4
Nikita Bushuev <mrbushik1@gmail.com>
2022-01-31 14:08:22 +0300

    добавил 2 урок

--- main.js
+++ main.js
@@ -1,15 +1,8 @@
-let title = "lesson02";
-let screens = "Простые, Сложные, Интерактивные";
+let title = "hello";
+let screens = 1080;
 let screenPrice = 999;
-let rollback = "16";
-let fullPrice = 1090;
-let adaptive = false;
-console.log('Тип данных title: ' + typeof title);
-console.log('Тип данных fullPrice: ' + typeof fullPrice);
-console.log('Тип данных adaptive: ' + typeof adaptive);
-console.log('Длина строки screens = ' + screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice +
-    '$ Стоимость разработки сайта ' + fullPrice + '$');
-console.log(screens.toLowerCase().split(' '));
-let workerRollback = (fullPrice * (rollback / 100));
-console.log('Откат посреднику за работу: ' + Math.round(workerRollback));
+const rollback = "back";
+const fullPrice = 1090;
+let adaptive = 320;
+console.log(title);
+alert(fullPrice);
