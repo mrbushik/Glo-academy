e5c13315948856c68d144726792f0287e8f607a8
Nikita Bushuev <mrbushik1@gmail.com>
2022-02-02 00:03:43 +0300

    начало урока

--- main.js
+++ main.js
@@ -1,37 +1,47 @@
 'use strict';
-let title = prompt('как называется ваш проект');
+let title = prompt('как называется ваш проект', "title");
 let screens = prompt('какие типы экранов надо разработать?',
     "Простые, Сложные, Интерактивные");
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let screenPrice = +prompt('Сколько будет стоить данная работа?', "15000");
 let adaptive = confirm("Нужен ли адаптив?");
-let service1 = prompt('Какие дополнительные услуги еще нужны?');
-let servise1Price = +prompt('сколько это будет стоить?');
-let service2 = prompt('Какие дополнительные услуги еще нужны?');
-let servise2Price = +prompt('сколько это будет стоить?');
+let service1 = prompt('Какие дополнительные услуги еще нужны?', "домен");
+let servise1Price = +prompt('сколько это будет стоить?', "100");
+let service2 = prompt('Какие дополнительные услуги еще нужны?', 'создание слайдера');
+let servise2Price = +prompt('сколько это будет стоить?', '500');
 let fullPrice = screenPrice + servise1Price + servise2Price;
 let rollback = "16";
 let workerRollback = (fullPrice * (rollback / 100));
-console.log('Откат посреднику за работу: ' + Math.round(workerRollback));
 let servisePresentPrise = fullPrice - workerRollback;
 servisePresentPrise = (Math.ceil(servisePresentPrise));
-console.log("Итоговая стоимость с учетом отката и округлением вверх " + servisePresentPrise);
-switch (true) {
-    case fullPrice >= 30000:
-        console.log("даем скидку 10%");
-        break;
-    case fullPrice >= 15000 && fullPrice < 30000:
-        console.log("даем скидку 5%");
-        break;
-    case fullPrice < 15000 && fullPrice > 0:
-        console.log("скидка не предусмотрена");
-        break;
-    case fullPrice <= 0:
-        console.log("Что-то пошло не так");
+let allServisePrices;
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return "даем скидку 10%";
+
+    } else if (fullPrice >= 15000 && price < 30000) {
+        return "даем скидку 5%";
+
+    } else if (fullPrice < 15000 && price > 0) {
+        return "скидка не предусмотрена";
+
+    } else {
+        return "Что-то пошло не так";
+    }
 }
+// const getAllServisePrices = function (price1, price2) {
+//     return price1 + price2;
+// }
+
+// console.log(getAllServisePrices(servise1Price, servise2Price + " test1111"));
+// allServisePrices = getAllServisePrices();
+
+console.log(getRollbackMessage(fullPrice))
 console.log('Тип данных title: ' + typeof title);
 console.log('Тип данных fullPrice: ' + typeof fullPrice);
 console.log('Тип данных adaptive: ' + typeof adaptive);
+console.log(screens.toLowerCase().split(' '));
+console.log("Итоговая стоимость с учетом отката и округлением вверх " + servisePresentPrise);
+console.log('Откат посреднику за работу: ' + Math.round(workerRollback));
 console.log('Длина строки screens = ' + screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice +
     '₽ Стоимость разработки сайта ' + fullPrice + '₽');
-console.log(screens.toLowerCase().split(' '));
