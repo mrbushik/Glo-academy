f642a46806d6d3eb17fabb145ae9afc0cfd4f4ba
Nikita Bushuev <mrbushik1@gmail.com>
2022-02-03 18:13:28 +0300

    работает но при строковых значениях NaN

--- main.js
+++ main.js
@@ -4,30 +4,32 @@ let screens;
 let screenPrice;
 let adaptive;
 let fullPrice;
-let rollback = "16";
-let workerRollback = fullPrice * (rollback / 100);
+let rollback = 16;
+let workerRollback;
 let servisePresentPrise = Math.ceil(fullPrice - workerRollback);
 let allServisePrices;
 let service1;
 let service2;
 let sum = 0;
 
+
+//проверка на число
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
+
 //вывод типа переменной
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable);
 }
-
+//вопросы 
 const asking = function () {
     title = prompt('как называется ваш проект', "КаЛьКулятор");
     screens = prompt('какие типы экранов надо разработать?',
         "Простые, Сложные, Интерактивные");
-    screenPrice = prompt('Сколько будет стоить данная работа?');
-    while (!isNumber(screenPrice)) {
+    do {
         screenPrice = +prompt('Сколько будет стоить данная работа?', 15000);
-    }
+    } while (!isNumber(screenPrice));
 
     adaptive = confirm("Нужен ли адаптив?");
 }
@@ -35,18 +37,31 @@ const asking = function () {
 //функция вычисления дополнительных услуг
 //выводит тип undefined
 const getAllServisePrices = function () {
+    let prise;
     for (let i = 0; i < 2; i++) {
         if (i === 0) {
             service1 = prompt('Какие дополнительные услуги еще нужны?', "домен");
         } else if (i === 1) {
             service2 = prompt('Какие дополнительные услуги еще нужны?', "хостинг");
         }
-        sum += +prompt('сколько это будет стоить?', 100);
+        // sum += prompt('сколько это будет стоить?', 100);
+        // while (!isNumber(sum)) {
+        //     sum += prompt('сколько это будет стоить?', 100);
+        // }
+        // sum += +prompt("Сколько это будет стоить?");
+        // while (isNumber(sum)) {
+        //     sum = prompt("Сколько это будет стоить?");
+        // }
+        do {
+            prise = +prompt('сколько это будет стоить?', 100);
+            sum += prise;
+        } while (!isNumber(prise));
     }
-    return sum;
+
+    return +sum;
 }
 
-
+//вывод скидки
 const getRollbackMessage = function (price) {
     if (price >= 30000) {
         return "даем скидку 10%";
@@ -67,6 +82,10 @@ function getFullPrice() {
     return screenPrice + sum;
 }
 
+const sumRollback = function () {
+    workerRollback = fullPrice * (rollback / 100);
+}
+
 // Вывод первой большой буквы остальные маленькие 
 const getTitle = function (a) {
     a = a.trim();
@@ -79,9 +98,14 @@ const getServicePresentPrices = function () {
 asking();
 getAllServisePrices(allServisePrices);
 fullPrice = getFullPrice();
+sumRollback();
+
+
 showTypeOf(`${getTitle(title)}`);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
+
+
 console.log(screens.toLowerCase().split(' '));
 console.log(getRollbackMessage(fullPrice));
 console.log("Стоимость с учетом отката сотруднику = " + getServicePresentPrices());
