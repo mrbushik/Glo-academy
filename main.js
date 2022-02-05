a38796940936aa467b2f6ca63bc2dbc4c2008eb3
Nikita Bushuev <mrbushik1@gmail.com>
2022-02-05 20:54:09 +0300

    работающее начало

--- main.js
+++ main.js
@@ -1,108 +1,126 @@
 'use strict';
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let fullPrice;
-let rollback = 16;
-let workerRollback;
-let servisePresentPrise = Math.ceil(fullPrice - workerRollback);
-let allServisePrices;
-let service1;
-let service2;
-let sum = 0;
-
-
-//проверка на число
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-//вывод типа переменной
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-}
-//вопросы 
-const asking = function () {
-    title = prompt('как называется ваш проект', "КаЛьКулятор");
-    screens = prompt('какие типы экранов надо разработать?',
-        "Простые, Сложные, Интерактивные");
-    do {
-        screenPrice = +prompt('Сколько будет стоить данная работа?', 15000);
-    } while (!isNumber(screenPrice));
+const appData = {
+    title: '',
+    screens: '',
+    screenPrice: 0,
+    adaptive: true,
+    fullPrice: 0,
+    rollback: 16,
+    workerRollback: 0,
+    servisePresentPrise: 0,
+    allServisePrices: 0,
+    service1: 0,
+    service2: 0,
+    sum: 0,
+    //проверка на число
 
-    adaptive = confirm("Нужен ли адаптив?");
-}
 
-//функция вычисления дополнительных услуг
-//выводит тип undefined
-const getAllServisePrices = function () {
-    let prise = 0;
-    for (let i = 0; i < 2; i++) {
-        if (i === 0) {
-            service1 = prompt('Какие дополнительные услуги еще нужны?', "домен");
-        } else if (i === 1) {
-            service2 = prompt('Какие дополнительные услуги еще нужны?', "хостинг");
-        }
-        // do {
-        //     prise = +prompt('сколько это будет стоить?', 100);
-        //     sum += prise;
-        // } while (!isNumber(prise));
-        prise = prompt('Сколько это будет стоить?');
+    asking: function () {
+        appData.title = prompt('как называется ваш проект', "КаЛьКулятор");
+        appData.screens = prompt('какие типы экранов надо разработать?',
+            "Простые, Сложные, Интерактивные");
         do {
-            prise = prompt('Сколько это будет стоить?');
-        } while (!isNumber(prise));
+            appData.screenPrice = +prompt('Сколько будет стоить данная работа?', 15000);
+        } while (!isNumber(appData.screenPrice));
+
+        appData.adaptive = confirm("Нужен ли адаптив?");
+    },
+
+    //функция вычисления дополнительных услуг
+    //выводит тип undefined
+    getAllServisePrices: function () {
+        let prise = 0;
+        for (let i = 0; i < 2; i++) {
+            if (i === 0) {
+                appData.service1 = prompt('Какие дополнительные услуги еще нужны?', "домен");
+            } else if (i === 1) {
+                appData.service2 = prompt('Какие дополнительные услуги еще нужны?', "хостинг");
+            }
+            do {
+                prise = prompt('Сколько это будет стоить?');
+            } while (!isNumber(prise));
+            appData.sum += +prise;
+        }
+
+        return +appData.sum;
+    },
+    //вывод скидки
+    getRollbackMessage: function (price) {
+        if (price >= 30000) {
+            return "даем скидку 10%";
+
+        } else if (appData.fullPrice >= 15000 && price < 30000) {
+            return "даем скидку 5%";
+
+        } else if (appData.fullPrice < 15000 && price > 0) {
+            return "скидка не предусмотрена";
+
+        } else {
+            return "Что-то пошло не так";
+        }
+    },
+
+    //вычисление полной стоимости
+    getFullPrice() {
+        return appData.screenPrice + appData.sum;
+    },
+    //откат работнику
+    sumRollback: function () {
+        appData.workerRollback = Math.ceil(appData.fullPrice * (appData.rollback / 100));
+    },
+    // Вывод первой большой буквы остальные маленькие 
+    getTitle: function (a) {
+        a = a.trim();
+        return a[0].toUpperCase() + a.slice(1).toLowerCase();
+    },
+    //вычисление стоимости с учетом отката
+    getServicePresentPrices: function () {
+        return appData.fullPrice - appData.workerRollback;
+    },
+    start: function () {
+        appData.asking();
+        appData.getAllServisePrices();
+        appData.fullPrice = appData.getFullPrice();
+        appData.sumRollback();
+        appData.allServisePrices = appData.getServicePresentPrices();
+        appData.title = appData.getTitle(appData.title);
 
     }
-
-    return +sum;
 }
 
-//вывод скидки
-const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return "даем скидку 10%";
 
-    } else if (fullPrice >= 15000 && price < 30000) {
-        return "даем скидку 5%";
-
-    } else if (fullPrice < 15000 && price > 0) {
-        return "скидка не предусмотрена";
-
-    } else {
-        return "Что-то пошло не так";
-    }
-}
-
-//вычисление полной стоимости
-function getFullPrice() {
-    return screenPrice + sum;
-}
-
-const sumRollback = function () {
-    workerRollback = fullPrice * (rollback / 100);
-}
-
-// Вывод первой большой буквы остальные маленькие 
-const getTitle = function (a) {
-    a = a.trim();
-    return a[0].toUpperCase() + a.slice(1).toLowerCase();
-}
-//вычисление стоимости с учетом отката
-const getServicePresentPrices = function () {
-    return fullPrice - workerRollback;
-}
-asking();
-getAllServisePrices(allServisePrices);
-fullPrice = getFullPrice();
-sumRollback();
+//вопросы 
 
 
-showTypeOf(`${getTitle(title)}`);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
 
 
-console.log(screens.toLowerCase().split(' '));
-console.log(getRollbackMessage(fullPrice));
-console.log("Стоимость с учетом отката сотруднику = " + getServicePresentPrices());
+
+
+
+
+
+
+
+
+
+
+
+
+appData.start();
+
+
+console.log(appData.title);
+console.log(appData.sum);
+console.log(appData.workerRollback)
+console.log(appData.allServisePrices)
+
+
+
+
+// console.log(screens.toLowerCase().split(' '));
+// console.log(getRollbackMessage(fullPrice));
+// console.log("Стоимость с учетом отката сотруднику = " + getServicePresentPrices());
